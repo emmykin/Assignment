38d0d320ea5fd701880267aac97e9c6c08585489
flexstargh <42805942+flexstargh@users.noreply.github.com>
2018-08-30 08:42:26 +0100

    an assignment

---
+++ main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n,i;
+    int c=1;
+    cout<<"Enter a number: ";
+    cin>>n;
+    for(i=2;i<n-1;i++)
+    {
+        if(n%i!=0)
+        {
+            continue;
+        }
+        else
+        {
+            c=0;
+        }
+    }
+    if(c==0)
+    {
+        cout<<"number is not prime";
+    }
+    else
+    {
+        cout<<"number is prime";
+    }
+
+    return 0;
+    }
+
+
